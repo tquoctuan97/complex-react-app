2839128f5ad6709d1bb9511e28ef14425d159163
TuanTQ <tquoctua97@gmail.com>
2021-09-24 02:56:45 +0700

    Show Home.js after login

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,9 +3,7 @@ import { Link } from 'react-router-dom'
 import HeaderLoggedIn from './HeaderLoggedIn'
 import HeaderLoggedOut from './HeaderLoggedOut'
 
-function Header() {
-  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem('complexappToken')))
-
+function Header(props) {
   return (
     <header className="header-bar bg-primary mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
@@ -15,7 +13,7 @@ function Header() {
             ComplexApp{' '}
           </Link>
         </h4>
-        {loggedIn ? <HeaderLoggedIn setLoggedIn={setLoggedIn} /> : <HeaderLoggedOut setLoggedIn={setLoggedIn} />}
+        {props.loggedIn ? <HeaderLoggedIn setLoggedIn={props.setLoggedIn} /> : <HeaderLoggedOut setLoggedIn={props.setLoggedIn} />}
       </div>
     </header>
   )
