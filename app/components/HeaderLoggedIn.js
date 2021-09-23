fe0c85d476901c059426f2749ea660dcd9a0d57f
TuanTQ <tquoctua97@gmail.com>
2021-09-24 02:48:12 +0700

    Add Avatar for HeaderLoggedIn

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -17,7 +17,7 @@ function HeaderLoggedIn(props) {
         <span className="chat-count-badge text-white"> </span>
       </span>
       <a href="#" className="mr-2">
-        <img className="small-header-avatar" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" />
+        <img className="small-header-avatar" src={localStorage.getItem('complexappAvatar')} />
       </a>
       <a className="btn btn-sm btn-success mr-2" href="/create-post">
         Create Post
